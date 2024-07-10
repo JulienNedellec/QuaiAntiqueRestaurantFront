7105921661a178fb4e7a65b974dec37fa6a56f25
JulienNedellec <nedellec.julien.78@gmail.com>
2024-07-10 12:24:10 +0200

    page reserver finish

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modifier votre mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Vos résevations", "/pages/reservations/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
